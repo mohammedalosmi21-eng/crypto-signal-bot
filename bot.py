c983ada0f2f82915fa21161258b927f534873b29
mohammedalosmi21-eng <mohammed.alosmi21@gmail.com>
2026-03-18 02:33:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-TOKEN = "TOKEN = os.getenv("BOT_TOKEN")"
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("البوت شغال.")
