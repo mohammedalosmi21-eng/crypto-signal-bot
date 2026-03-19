af64126e104cbe1c88c098e6b1f7890200e093e6
mohammedalosmi21-eng <mohammed.alosmi21@gmail.com>
2026-03-19 06:28:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ import asyncio
 # CONFIG
 # ─────────────────────────────────────────────
 
-TOKEN = os.getenv("8713422124:AAFv3kYJgGrxEYS8ogtXJpqpOYekp7-OwVo")
+TOKEN = os.getenv("BOT_TOKEN")
 OWNER_CHAT_ID = 760930914
 
 CHAIN_FILTER = "bsc"
